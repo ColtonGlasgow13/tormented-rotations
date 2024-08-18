2dd6b33e762ddf9bf302fd7810b4b95106e00778
Cole Glasgow <coleglasgow13@gmail.com>
2024-08-18 17:38:10 -0400

    Maybe fix main

--- components/ConfigureAmplifyClientSide.ts
+++ components/ConfigureAmplifyClientSide.ts
@@ -1,9 +1,11 @@
 "use client";
 
+import config from "@/amplify_outputs.json";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs, { ssr: true });
+Amplify.configure(config, {
+  ssr: true,
+});
 
 export default function ConfigureAmplifyClientSide() {
   return null;
