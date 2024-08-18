0e6a0f8bdc9df20dd76f6eb121f8e56d6ca27e40
Cole Glasgow <coleglasgow13@gmail.com>
2024-08-18 10:12:58 -0400

    Remove garbage

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 import DiabloButton from "./DiabloButton";
-import Modal from "./Modal";
 import ConfigureAmplifyClientSide from "./ConfigureAmplifyClientSide";
 import DiabloInput from "./DiabloInput";
 
-export { DiabloButton, Modal, DiabloInput, ConfigureAmplifyClientSide };
+export { DiabloButton, DiabloInput, ConfigureAmplifyClientSide };
