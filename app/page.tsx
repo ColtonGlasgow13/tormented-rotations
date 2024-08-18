daf21ae29bcd30887b358d48be17648a84010a41
Cole Glasgow <coleglasgow13@gmail.com>
2024-08-18 09:42:57 -0400

    Add a basic button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
+import { DiabloButton } from "@/components";
 import { client } from "@/utils/amplifyUtils";
 
 export default function Home() {
-  const createRequest = async () => {
-    await client.models.Request.create({ username: "testUsername" });
-  };
+  // const createRequest = async () => {
+  //   await client.models.Request.create({ username: "testUsername" });
+  // };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-xl">Tormented Rotations</h1>
       <div className="flex items-start mt-8">
-        <button className="bg-red-400" onClick={createRequest}>
-          Testing Button
-        </button>
+        <DiabloButton label="Test Button" />
       </div>
     </main>
   );
---
+++ components/index.ts
@@ -0,0 +1,3 @@
+import DiabloButton from "./DiabloButton";
+
+export { DiabloButton };
