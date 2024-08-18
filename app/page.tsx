9712c90995b097672ad1ce0cf984cfe9ef118096
Cole Glasgow <coleglasgow13@gmail.com>
2024-08-18 11:40:53 -0400

    Disable inputs once queued

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,34 @@ import { useState } from "react";
 
 export default function Home() {
   const [username, setUsername] = useState<string>("");
+  const [currentlyInQueue, setCurrentlyInQueue] = useState<boolean>(false);
 
   const createRequest = async () => {
     try {
+      setCurrentlyInQueue(true);
       await client.models.Request.create({ username });
-      console.log(`Request created for username: ${username}`);
     } catch (error) {
       console.error("Failed to create request:", error);
+      setCurrentlyInQueue(false);
     }
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-xl">Tormented Rotations</h1>
+      <h1 className="text-5xl font-black">Tormented Rotations</h1>
       <div className="flex items-start mt-8">
-        <DiabloInput value={username} onChange={setUsername} />
+        <DiabloInput
+          value={username}
+          onChange={setUsername}
+          disabled={currentlyInQueue}
+        />
       </div>
       <div className="flex items-start mt-8">
-        <DiabloButton label="Test Button" onClick={createRequest} />
+        <DiabloButton
+          label="Test Button"
+          onClick={createRequest}
+          disabled={currentlyInQueue}
+        />
       </div>
     </main>
   );
--- components/DiabloInput.tsx
+++ components/DiabloInput.tsx
@@ -6,23 +6,24 @@ interface DiabloInputProps {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
+  disabled?: boolean;
 }
 
 const DiabloInput: React.FC<DiabloInputProps> = ({
   value,
   onChange,
   placeholder = "ILikeTrains#12345",
+  disabled = false,
 }) => {
   const [isFocused, setIsFocused] = useState(false);
   const [isValid, setIsValid] = useState(true);
 
   const validateInput = (input: string) => {
-    // Regular expression to validate that the input ends with a pound sign followed by 4 or more digits
     const regex = /#\d{4,}$/;
     return regex.test(input);
   };
 
-  const handleFocus = () => setIsFocused(true);
+  const handleFocus = () => !disabled && setIsFocused(true);
   const handleBlur = () => {
     setIsFocused(false);
     setIsValid(validateInput(value));
@@ -30,13 +31,15 @@ const DiabloInput: React.FC<DiabloInputProps> = ({
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
-    onChange(newValue);
-    setIsValid(validateInput(newValue));
+    if (!disabled) {
+      onChange(newValue);
+      setIsValid(validateInput(newValue));
+    }
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      (e.target as HTMLInputElement).blur(); // Deselect the input on Enter
+    if (!disabled && e.key === "Enter") {
+      (e.target as HTMLInputElement).blur();
       setIsValid(validateInput(value));
     }
   };
@@ -44,19 +47,24 @@ const DiabloInput: React.FC<DiabloInputProps> = ({
   return (
     <div className="relative">
       <input
-        className={`w-full py-3 px-6 text-lg font-bold text-gray-100 transition-transform duration-200 transform ${
-          isFocused ? "scale-105 border-red-600" : "scale-100"
-        } ${isValid ? "bg-gray-900" : "bg-red-900"} border-2 ${
-          isValid ? "border-red-800" : "border-red-600"
-        } placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-800`}
+        className={`w-full py-3 px-6 text-lg font-bold transition-transform duration-200 transform ${
+          disabled
+            ? "bg-gray-600 text-gray-400 border-gray-600 cursor-not-allowed"
+            : `${isFocused ? "scale-105 border-red-600" : "scale-100"} ${
+                isValid ? "bg-gray-900" : "bg-red-900"
+              } border-2 ${
+                isValid ? "border-red-800" : "border-red-600"
+              } text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-800`
+        }`}
         value={value}
         onChange={handleChange}
         onFocus={handleFocus}
         onBlur={handleBlur}
         onKeyDown={handleKeyDown}
         placeholder={placeholder}
+        disabled={disabled}
       />
-      {!isValid && (
+      {!isValid && !disabled && (
         <p className="text-red-600 mt-1">
           Use your full Battle.net username, eg. "ILoveDiablo#1234"
         </p>
